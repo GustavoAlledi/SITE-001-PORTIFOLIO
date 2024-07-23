032b75e2f4a1bc2a6d44532a0d4439662ac0db0e
Gustavo Alledi <162318257+GustavoAlledi@users.noreply.github.com>
2024-07-23 19:51:06 -0300

    Site modelo criado
    criação de site modelo carrossel

---
+++ zznovo site carrocel/scripts/scripts.js
@@ -0,0 +1,24 @@
+let btnNext = document.querySelector('.next')
+let btnBack = document.querySelector('.back')
+
+let container = document.querySelector('.container')
+let list = document.querySelector('.container .list')
+let thumb = document.querySelector('.container .thumb')
+
+btnNext.onclick = () => moveItemsOnClick('next')
+btnBack.onclick = () => moveItemsOnClick('back')
+
+function moveItemsOnClick(type){
+
+    let listItems = document.querySelectorAll('.list .list-item')
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+
+    console.log(listItems);
+    console.log(thumbItems);
+
+    if(type === 'next'){
+
+    } else {
+
+    }
+}
